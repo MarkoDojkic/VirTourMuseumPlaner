902fc6bda9ae71d8d23ff5e595d3472f9f00d652
Marko Dojkić <marko.dojkic@gmail.com>
2022-01-10 18:59:51 +0100

    Finished implementing filtering exhibitions

--- src/app/main/exhibitions/exhibitions.component.ts
+++ src/app/main/exhibitions/exhibitions.component.ts
@@ -6,6 +6,7 @@ import { LabelType, Options } from '@angular-slider/ngx-slider';
 import { Router } from '@angular/router';
 import { Exhibit } from 'src/app/model/exhibit';
 import Swal from 'sweetalert2';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-exhibitions',
@@ -20,20 +21,15 @@ export class ExhibitionComponent implements OnInit {
   carouselResponsiveOptions;
 
   //Filtering values
-  //Filtering values
   minPrice!: number;
   maxPrice!: number;
   minExhibitCount!: number;
   maxExhibitCount!: number;
   minTime!: number;
   maxTime!: number;
-  /*minInStock: number;
-  voltages: Map<string, boolean>;
-  amperages: Map<string, boolean>;
-  wattages: Map<string, boolean>;
-  amperhours: Map<string, boolean>;
-  minDrivingRange: number;
-  maxDrivingRange: number;*/
+  exhibitionTypes!: Map<string, boolean>;
+  exhibitTypes!: Map<string, boolean>;
+  minRating: number = 0;
   priceSliderOptions: Options = {
     "floor": 0,
     "ceil": 0,
@@ -52,7 +48,6 @@ export class ExhibitionComponent implements OnInit {
     "step": 0,
     "hideLimitLabels": true
   };
-  //minRating: number = 0;
 
   constructor(private exhibitionService: ExhibitionService, private ns: NotifierService, private router: Router) {
     this.carouselResponsiveOptions = [
@@ -110,172 +105,13 @@ export class ExhibitionComponent implements OnInit {
       this.exhibitions = resolve;
     }).catch((reject) => console.log(reject));
     setTimeout(() => { /* To give time to firestore to get items */
-      /*this.selectedCategory = category;
-      this.voltages = this.amperages = this.wattages = this.amperhours = null;
+      this.exhibitionTypes = new Map(this.exhibitions.map(exhibition => {
+        return [exhibition.type, true];
+      }));
 
-      switch (this.selectedCategory) {
-        case "Монокристални":
-          this.rowHeight = "1:2.15";
-          this.voltages = new Map([["12V", true], ["24V/36V", true], ["48V", true]]);
-          break;
-        case "Поликристални":
-          this.rowHeight = "1:2.1";
-          this.voltages = new Map([["12V", true], ["24V", true], ["36V", true]]);
-          break;
-        case "Аморфни":
-          this.rowHeight = "1:2.22";
-          this.voltages = new Map([["12V", true], ["24V", true], ["36V", true], ["48V", true], ["84V", true]]);
-          break;
-        case "PWM":
-          this.rowHeight = "1:2.58";
-          this.voltages = new Map([["12V/24V", true], ["48V", true]]);
-          this.amperages = new Map(this.items.map(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Максимална струја пуњења: "),
-              temp.lastIndexOf(" Подразумевани напон искључења"))
-            temp = temp.slice(26, temp.length);
-            return [temp, true];
-          }));
-          break;
-        case "MPPT":
-          this.rowHeight = "1:2.22";
-          this.amperages = new Map(this.items.map(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Максимална струја пуњења: "),
-              temp.lastIndexOf(" Ефективност конверзије"))
-            temp = temp.slice(26, temp.length);
-            return [temp, true];
-          }));
-          break;
-        case "OFF-Grid":
-          this.rowHeight = "1:2.02";
-          this.wattages = new Map(this.items.map(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Излазна снага: "),
-              temp.lastIndexOf(" Излазна снага у"))
-            temp = temp.slice(15, temp.length);
-            return [temp, true];
-          }));
-          break;
-        case "ON-Grid":
-          this.rowHeight = "1:2.22";
-          this.wattages = new Map(this.items.map(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Максимала препоручена снага система:"),
-              temp.lastIndexOf(" Улазни напон:"))
-            temp = temp.slice(36, temp.length);
-            return [temp, true];
-          }));
-          break;
-        case "Хибридни":
-          this.rowHeight = "1:2.36";
-          this.wattages = new Map(this.items.map(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Снага система: "),
-              temp.lastIndexOf(" Снага система у пику"))
-            temp = temp.slice(15, temp.length);
-            return [temp, true];
-          }));
-          break;
-        case "Хоризонтални":
-          this.rowHeight = "1:2.84";
-          this.wattages = new Map(this.items.map(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Излазна снага: "),
-              temp.lastIndexOf(" Максимална излазна снага"))
-            temp = temp.slice(15, temp.length);
-            return [temp, true];
-          }));
-          break;
-        case "Вертикални":
-          this.rowHeight = "1:2.75";
-          this.wattages = new Map(this.items.map(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Излазна снага: "),
-              temp.lastIndexOf(" Максимална излазна снага"))
-            temp = temp.slice(15, temp.length);
-            return [temp, true];
-          }));
-          break;
-        case "Оловни":
-          this.rowHeight = "1:1.85";
-          this.amperhours = new Map(this.items.map(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Максимални капацитет: "),
-              temp.lastIndexOf(" Радна температура"))
-            temp = temp.slice(22, temp.length);
-            return [temp, true];
-          }));
-          break;
-        case "Никл базирани":
-          this.rowHeight = "1:1.9";
-          this.amperhours = new Map(this.items.map(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Максимални капацитет: "),
-              temp.lastIndexOf(" Радна температура"))
-            temp = temp.slice(22, temp.length);
-            return [temp, true];
-          }));
-          break;
-        case "Литијумски":
-          this.rowHeight = "1:1.97";
-          this.amperhours = new Map(this.items.map(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Максимални капацитет: "),
-              temp.lastIndexOf(" Максимална дозвољена"))
-            temp = temp.slice(22, temp.length);
-            return [temp, true];
-          }));
-          break;
-        case "Специјални":
-          this.rowHeight = "1:2.1";
-          this.amperhours = new Map(this.items.map(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Максимални капацитет: "),
-              temp.lastIndexOf(" Максимална дозвољена"))
-            temp = temp.slice(22, temp.length);
-            return [temp, true];
-          }));
-          break;
-        case "Електрична возила":
-          this.rowHeight = "1:1.85";
-          const minDR = Math.min.apply(Math, this.items.map(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Домет при пуној батерији: "),
-              temp.lastIndexOf(" km Број путника:"))
-            temp = temp.slice(26, temp.length);
-            return parseInt(temp);
-          }));
-          const maxDR = Math.max.apply(Math, this.items.map(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Домет при пуној батерији: "),
-              temp.lastIndexOf(" km Број путника:"))
-            temp = temp.slice(26, temp.length);
-            return parseInt(temp);
-          }));
-
-          this.minDrivingRange = minDR;
-          this.maxDrivingRange = maxDR;
-
-          this.priceSliderOptionsDrivingRange = {
-            floor: minDR,
-            ceil: maxDR,
-            step: (maxDR - minDR) / 10,
-            translate: (value: number, label: LabelType): string => {
-              switch (label) {
-                case LabelType.Low:
-                  return "<b>" + value + " километара</b>";
-                case LabelType.High:
-                  return "<b>" + value + " километара</b>";
-                default:
-                  return value + " километара";
-              }
-            },
-            hideLimitLabels: true
-          };
-          break;
-        default: this.rowHeight = "1:1";
-      }*/
+      this.exhibitTypes = new Map(this.exhibitions.map(exhibition => {
+        return [exhibition.exhibitsType, true];
+      }));
 
       this.filteredExhibitions = this.exhibitions;
 
@@ -350,219 +186,59 @@ export class ExhibitionComponent implements OnInit {
   applyFilters(): void {
     this.filteredExhibitions = this.exhibitions.filter(exhibition => // First filter -> use all items array
       this.returnTotalCost(exhibition.exhibits) >= this.minPrice && this.returnTotalCost(exhibition.exhibits) <= this.maxPrice
-    )
-
-    this.filteredExhibitions = this.filteredExhibitions.filter(exhibition => // First filter -> use all items array
+    ).filter(exhibition =>
       this.returnTotalTime(exhibition.exhibits) >= this.minTime && this.returnTotalTime(exhibition.exhibits) <= this.maxTime
-    )
-
-    this.filteredExhibitions = this.filteredExhibitions.filter(exhibition => // First filter -> use all items array
+    ).filter(exhibition =>
       exhibition.exhibits.length >= this.minExhibitCount && exhibition.exhibits.length <= this.maxExhibitCount
-    )
+    );
 
-    /*if (this.minInStock != null || this.minInStock != undefined)
-      this.filteredItems = this.filteredItems.filter(item => item.leftInStock >= this.minInStock); //Every other filter -> use filtered items array
-  
-    this.filteredItems.forEach(item => {
+    for (const key of Array.from(this.exhibitionTypes.keys())) {
+      if (!this.exhibitionTypes.get(key)) {
+        this.filteredExhibitions = this.filteredExhibitions.filter(exhibition => exhibition.type !== key);
+      }
+    }
+
+    for (const key of Array.from(this.exhibitTypes.keys())) {
+      if (!this.exhibitTypes.get(key)) {
+        this.filteredExhibitions = this.filteredExhibitions.filter(exhibition => exhibition.exhibitsType !== key);
+      }
+    }
+
+    this.filteredExhibitions.forEach(exhibition => {
       var sumOfRatings: number = 0;
       var totalReviews: number = 0;
-  
-      this.fs.getAllReviewsForProduct(item.id).then(reviews => {
+
+      exhibition.exhibits.forEach(e => this.exhibitionService.getReviewsForExhibit(e.id).then(reviews => {
         reviews.forEach(review => {
-          sumOfRatings += review.rating
+          sumOfRatings += review.rating;
           totalReviews++;
         });
-      }).then(() => {
-        if (totalReviews !== 0 && Math.round(sumOfRatings / totalReviews) < this.minRating)
-          this.filteredItems.splice(this.filteredItems.findIndex(currentItem => currentItem.id === item.id), 1)
-      });
-    });
-  
-    if (this.selectedCategory === "Монокристални" || this.selectedCategory === "Поликристални" || this.selectedCategory === "Аморфни") {
-      for (const key of Array.from(this.voltages.keys())) {
-        if (key === "12V" && !this.voltages.get(key)) this.filterOutVoltage(12, 24);
-        if (key === "24V" && !this.voltages.get(key)) this.filterOutVoltage(24, 36);
-        if (key === "24V/36V" && !this.voltages.get(key)) this.filterOutVoltage(24, 48);
-        if (key === "36V" && !this.voltages.get(key)) this.filterOutVoltage(36, 48);
-        if (key === "48V" && !this.voltages.get(key)) this.filterOutVoltage(48, 84);
-        if (key === "84V" && !this.voltages.get(key)) this.filterOutVoltage(84, 100);
-      }
-    }
-  
-    if (this.selectedCategory === "PWM") {
-      for (const key of Array.from(this.voltages.keys())) {
-        if (!this.voltages.get(key)) {
-          this.filteredItems = this.filteredItems.filter(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Напон система: "),
-              temp.lastIndexOf(" Максимални улазни напон:"))
-            temp = temp.slice(15, temp.length);
-            return temp !== key;
-          });
-        }
-      }
-  
-      for (const key of Array.from(this.amperages.keys())) {
-        if (!this.amperages.get(key)) {
-          this.filteredItems = this.filteredItems.filter(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Максимална струја пуњења: "),
-              temp.lastIndexOf(" Подразумевани напон искључења"))
-            temp = temp.slice(26, temp.length);
-            return temp !== key;
-          });
-        }
-      }
-    }
-  
-    if (this.selectedCategory === "MPPT") {
-      for (const key of Array.from(this.amperages.keys())) {
-        if (!this.amperages.get(key)) {
-          this.filteredItems = this.filteredItems.filter(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Максимална струја пуњења: "),
-              temp.lastIndexOf(" Ефективност конверзије"))
-            temp = temp.slice(26, temp.length);
-            return temp !== key;
-          });
-        }
-      }
-    }
-  
-    if (this.selectedCategory === "OFF-Grid") {
-      for (const key of Array.from(this.wattages.keys())) {
-        if (!this.wattages.get(key)) {
-          this.filteredItems = this.filteredItems.filter(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Излазна снага: "),
-              temp.lastIndexOf(" Излазна снага у"))
-            temp = temp.slice(15, temp.length);
-            return temp !== key;
-          });
-        }
-      }
-    }
-  
-    if (this.selectedCategory === "ON-Grid") {
-      for (const key of Array.from(this.wattages.keys())) {
-        if (!this.wattages.get(key)) {
-          this.filteredItems = this.filteredItems.filter(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Максимала препоручена снага система:"),
-              temp.lastIndexOf(" Улазни напон:"))
-            temp = temp.slice(36, temp.length);
-            return temp !== key;
-          });
-        }
-      }
-    }
-  
-    if (this.selectedCategory === "Хибридни") {
-      for (const key of Array.from(this.wattages.keys())) {
-        if (!this.wattages.get(key)) {
-          this.filteredItems = this.filteredItems.filter(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Снага система: "),
-              temp.lastIndexOf(" Снага система у пику"))
-            temp = temp.slice(15, temp.length);
-            return temp !== key;
-          });
-        }
-      }
-    }
-  
-    if (this.selectedCategory === "Хоризонтални" || this.selectedCategory === "Вертикални") {
-      for (const key of Array.from(this.wattages.keys())) {
-        if (!this.wattages.get(key)) {
-          this.filteredItems = this.filteredItems.filter(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Излазна снага: "),
-              temp.lastIndexOf(" Максимална излазна снага"))
-            temp = temp.slice(15, temp.length);
-            return temp !== key;
-          });
-        }
-      }
-    }
-  
-    if (this.selectedCategory === "Оловни" || this.selectedCategory === "Никл базирани") {
-      for (const key of Array.from(this.amperhours.keys())) {
-        if (!this.amperhours.get(key)) {
-          this.filteredItems = this.filteredItems.filter(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Максимални капацитет: "),
-              temp.lastIndexOf(" Радна температура"))
-            temp = temp.slice(22, temp.length);
-            return temp !== key;
-          });
-        }
-      }
-    }
-  
-    if (this.selectedCategory === "Литијумски" || this.selectedCategory === "Специјални") {
-      for (const key of Array.from(this.amperhours.keys())) {
-        if (!this.amperhours.get(key)) {
-          this.filteredItems = this.filteredItems.filter(item => {
-            var temp = item.description;
-            temp = temp.slice(temp.lastIndexOf("Максимални капацитет: "),
-              temp.lastIndexOf(" Максимална дозвољена"))
-            temp = temp.slice(22, temp.length);
-            return temp !== key;
-          });
-        }
-      }
-    }
-  
-    if (this.selectedCategory === "Електрична возила") {
-      this.filteredItems = this.filteredItems.filter(item => {
-        var temp = item.description;
-        temp = temp.slice(temp.lastIndexOf("Домет при пуној батерији: "),
-          temp.lastIndexOf(" km Број путника:"))
-        temp = temp.slice(26, temp.length);
-        return parseInt(temp) >= this.minDrivingRange && parseInt(temp) <= this.maxDrivingRange;
-      })
-    }
-  } */
+      }));
 
-    /*updateVoltages(key: string, value: MatCheckboxChange): void {
-      this.voltages.set(key, value.checked);
-      this.applyFilters();
-    }
-    
-    updateAmperages(key: string, value: MatCheckboxChange): void {
-      this.amperages.set(key, value.checked);
-      this.applyFilters();
-    }
-    
-    updateWattages(key: string, value: MatCheckboxChange): void {
-      this.wattages.set(key, value.checked);
-      this.applyFilters();
-    }
-    
-    updateAmperhours(key: string, value: MatCheckboxChange): void {
-      this.amperhours.set(key, value.checked);
-      this.applyFilters();
-    }
-    
-    updateRating(minRating: number): void {
-      this.minRating = minRating;
-      for (let i = 1; i <= 5; i++) {
-        document.querySelector("#shopRating_" + i).innerHTML = i <= minRating ? "star" : "star_outline";
-      }
-    }
-    
-    filterOutVoltage(voltageMin: number, voltageMax: number): void {
-      this.filteredItems = this.filteredItems.filter(item => {
-        var temp = item.description;
-        temp = temp.slice(temp.lastIndexOf("Максимални радни напон: "),
-          temp.lastIndexOf("V Струја кратког споја:"))
-        temp = temp.slice(24, temp.length);
-    
-        return parseInt(temp) < voltageMin || parseInt(temp) > voltageMax;
-      });
-    }
-    */
+      setTimeout(() => {
+        if (totalReviews !== 0 && Math.round(sumOfRatings / totalReviews) < this.minRating)
+          this.filteredExhibitions.splice(this.filteredExhibitions.findIndex(fE => fE.id === exhibition.id), 1)
+      }, 1); //wait forEach
+    });
   }
+
+  updateExhibitionTypes(key: string, value: MatCheckboxChange): void {
+    this.exhibitionTypes.set(key, value.checked);
+    this.applyFilters();
+  }
+
+  updateExhibitTypes(key: string, value: MatCheckboxChange): void {
+    this.exhibitTypes.set(key, value.checked);
+    this.applyFilters();
+  }
+
+  updateRating(minRating: number): void {
+    this.minRating = minRating;
+    for (let i = 1; i <= 10; i++) {
+      document.querySelector("#exhibitionRating_" + i)!.innerHTML = i <= minRating ? "star" : "star_outline";
+    }
+  }
+
   addToPlaner(exhibitId: string) {
     if (sessionStorage.getItem("loggedInUser") == null) {
       Swal.fire({
@@ -609,6 +285,7 @@ export class ExhibitionComponent implements OnInit {
       this.ns.notify("info", "Стање у корпи за производ „" + product.title + "“ је: " + newQuantity + " комад(а)");
     }); */
   }
+
 
   showReviews(exhibitId: string) {
     this.exhibitionService.getReviewsForExhibit(exhibitId).then(response => {
@@ -666,5 +343,3 @@ export class ExhibitionComponent implements OnInit {
     });
   }
 }
-
-//export { Item, CategoryNode, FlatNode }; //To resolve additional export request by Angular
