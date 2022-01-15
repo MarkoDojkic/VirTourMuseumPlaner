1db0d73f91f640a0c4c57f2db067a593af900a10
Marko Dojkić <marko.dojkic@gmail.com>
2022-01-15 20:40:39 +0100

    Stepper fixes, added tour search and cancelation

--- src/app/main/planer/planer.component.ts
+++ src/app/main/planer/planer.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent, CalendarView } from 'angular-calendar';
 import * as moment from 'moment';
 import { TourService } from '../../services/tour/tour.service';
 import serbianLocale from '@angular/common/locales/sr-Cyrl';
 import { registerLocaleData } from '@angular/common';
 import { isSameMonth, isSameDay } from 'date-fns';
-import { MatStepper } from '@angular/material/stepper';
+import { MatStepper, StepperOrientation } from '@angular/material/stepper';
 import { Tour } from 'src/app/model/tour';
 import { Review } from 'src/app/model/review';
+import { Observable } from 'rxjs/internal/Observable';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-planer',
@@ -23,15 +26,24 @@ export class PlanerComponent implements OnInit {
   activeDayIsOpen: boolean = false;
   @ViewChild('planerStepper') private planerStepper!: MatStepper;
   currentViewingTour: Tour | undefined = undefined;
+  stepperOrientation!: Observable<StepperOrientation>;
 
-  constructor(private tourService: TourService) { }
+  constructor(private tourService: TourService, breakpointObserver: BreakpointObserver) {
+    this.stepperOrientation = breakpointObserver
+      .observe('(min-width: 800px)')
+      .pipe(map(({ matches }) => (matches ? 'horizontal' : 'vertical')));
+  }
 
   ngOnInit(): void {
     registerLocaleData(serbianLocale);
     this.getPlaner();
+    setTimeout(() => {
+      this.viewDate = new Date();
+    }, 100); //Wait for planer to be fetched
   }
 
   getPlaner(): void {
+    this.events = [];
     this.tourService.getToursData().then(resolve => {
       resolve.forEach(tour => {
         var totalTime = moment.duration(tour.exhibits.map(e => e.time).reduce((totalTime, time) => totalTime += time, 0), "minutes").format({ precision: 0, template: "hh:mm" });
@@ -51,17 +63,20 @@ export class PlanerComponent implements OnInit {
           end: tourEnd,
           color: this.getColorByStatus(tour.status),
           draggable: true,
-          actions: this.getActions(tour, tour.status)
+          actions: this.getActions(tour, tourStart),
+          meta: {
+            id: tour.status
+          }
         });
       });
     }).catch(reject => console.log(reject));
   }
 
-  getActions(tour: Tour, status: string): CalendarEventAction[] {
-    switch (status) {
-      case "Текућа": return [
+  getActions(tour: Tour, date: Date): CalendarEventAction[] {
+    switch (tour.status) {
+      case "Текући": return [
         {
-          label: '<i class="material-icons">&enspedit</i>',
+          label: '<i class="material-icons">edit</i>',
           a11yLabel: 'Edit',
           onClick: ({ event }: { event: CalendarEvent }): void => {
             console.log("----EDIT REQUESTED----");
@@ -70,13 +85,16 @@ export class PlanerComponent implements OnInit {
           },
         },
         {
-          label: '<i class="material-icons">&enspdelete_forever</i>',
+          label: '<i class="material-icons">delete_forever</i>',
           a11yLabel: 'Cancel',
           onClick: ({ event }: { event: CalendarEvent }): void => {
-            console.log("----CANCELATION REQUESTED----");
-            console.log(event);
-            /* this.events = this.events.filter((iEvent) => iEvent !== event);
-            this.handleEvent('Deleted', event); */
+            this.tourService.cancelTour(tour.id);
+            setTimeout(() => {
+              this.getPlaner();
+            }, 200);
+            setTimeout(() => {
+              this.viewDate = date!;
+            }, 300);
           },
         },
       ];
@@ -86,13 +104,11 @@ export class PlanerComponent implements OnInit {
           a11yLabel: 'View',
           onClick: (): void => {
             this.currentViewingTour = tour;
-            console.log(this.currentViewingTour.reviews);
-            this.setAverageRating(this.currentViewingTour.reviews);
             this.planerStepper.next();
           },
         }
       ];
-      case "Завршена": return [
+      case "Завршен": return [
         {
           label: '<i class="material-icons">edit</i>',
           a11yLabel: 'Edit',
@@ -109,12 +125,10 @@ export class PlanerComponent implements OnInit {
 
   filterTours(event: KeyboardEvent): void {
     var filterValue = (<HTMLInputElement>event.target).value;
-    /* if (filterValue === undefined || filterValue.length === 0)
-      this.orders.data = this.listedOrders;
-    else
-      this.orders.data = this.listedOrders.filter(order => JSON.stringify(Object.values(order)).includes(filterValue, 0));
-    this.updatePageSizeOptions(); */
-    //Example of search string: ["TE561FLEyixbxHtTjvmi","2020-11-04T03:06:35.000Z",{"item_117":"5$237500","item_52":"4$16700","item_35":"5$19200","item_21":"13$19200","item_36":"13$22300","item_71":"15$21000"},"Булевар Милутина Миланковића 25, Нови Београд","Пошта","Текућа",2657760]
+    this.getPlaner(); //Reload planer
+    setTimeout(() => {
+      this.events = this.events.filter(event => JSON.stringify(event).includes(filterValue, 0));
+    }, 100); //Wait for planer to be fetched
   }
 
   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
@@ -171,7 +185,6 @@ export class PlanerComponent implements OnInit {
   }
 
   setAverageRating(reviews: Array<Review>): void {
-
     var html = "";
     var average = reviews.map(r => r.rating as number).reduce((total, rating) => total += rating, 0) / reviews.length;
 
@@ -180,7 +193,7 @@ export class PlanerComponent implements OnInit {
       html += i > average ? "star_outline" : "star";
       html += `</mat-icon>`;
     }
-    console.log(html);
+
     document.querySelector("#averageRatingPlaceholder")!.innerHTML = html;
   }
 }
